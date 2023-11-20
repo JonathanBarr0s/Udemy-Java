b990b70081ed965ff21252df9615c2f7ca70ae17
Jonathan Barros <jonathan.sbarros@hotmail.com>
2023-11-19 22:57:45 -0300

    Update ConversorDeMoeda.java

--- Seção 08 - Introdução à POO/04. Conversor Dolar Para Real/ConvertendoDolarReal/src/Entities/ConversorDeMoeda.java
+++ Seção 08 - Introdução à POO/04. Conversor Dolar Para Real/ConvertendoDolarReal/src/Entities/ConversorDeMoeda.java
@@ -2,8 +2,6 @@ package Entities;
 
 public class ConversorDeMoeda {
 
-	// public static double preco, dolar;
-
 	public static final double IOF = 0.06;
 
 	public static double conversor(double preco, double dolar) {
